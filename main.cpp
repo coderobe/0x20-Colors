05d269c4eed5f68f971752ed76245e292a64d409
Robin Broda <robin@broda.me>
2016-10-09 19:11:50 +0200

    Split utility functions to their own source file

--- main.cpp
+++ main.cpp
@@ -34,38 +34,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 
 #include "stb_image.h"
-
-static void die(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    va_end(ap);
-    fputs("\n", stderr);
-    exit(EXIT_FAILURE);
-}
-
-static struct nk_image icon_load(const char *filename) {
-    int x, y, n;
-    GLuint tex;
-    unsigned char *data = stbi_load(filename, &x, &y, &n, 0);
-    if (!data) die("[GLEW]: failed to load image: %s", filename);
-
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    // FIXME: figure out why this segfaults
-    //glGenerateMipmap(GL_TEXTURE_2D);
-    stbi_image_free(data);
-    return nk_image_id((int) tex);
-}
-
-static void error_callback(int e, const char *d) {
-    printf("Error %d: %s\n", e, d);
-}
+#include "util.h"
 
 int main(void) {
     /* Platform */
@@ -102,6 +71,7 @@ int main(void) {
         nk_glfw3_new_frame();
 
         /* GUI */
+        /*
         {
             struct nk_panel layout;
             if (nk_begin(ctx, &layout, "0xNative image test", nk_rect(320, 50, 275, 610),
@@ -115,6 +85,7 @@ int main(void) {
             }
             nk_end(ctx);
         }
+        */
 
         /* Draw */
         {
